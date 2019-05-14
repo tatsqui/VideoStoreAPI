1fc4889909af0823f62d8beca975f5261cb6e472
Tatiana Quintana <tatsqui@gmail.com>
2019-05-14 15:33:01 -0700

    wrote tests for movie#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,12 +2,18 @@ class MoviesController < ApplicationController
 
   def index
     movies = Movie.all
-    render json: movies.as_json(only: [:available_inventory :id, :title, :inventory, :overview, :release_date]), status: :ok
+    render json: movies.as_json(only: [:available_inventory, :id, :title, :inventory, :overview, :release_date]), status: :ok
   end
   
-  # def show
+  def show
+    movie = Movie.find_by(id: params[:id])
 
-  # end
+    if movie
+      render json: movie.as_json(only: [:available_inventory, :id, :title, :inventory, :overview, :release_date]), status: :ok
+    else
+      render json: { errors: ["The movie with id #{params[:id]} could not be found"] }, status: :not_found
+    end
+  end
 
   private
 
