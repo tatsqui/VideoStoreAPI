6cbfd75efea95bc47ddda0129653715352629551
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-14 15:10:45 -0700

    updated what is returned in JSON to include movie count

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]),
+    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
            status: :ok
   end
 
