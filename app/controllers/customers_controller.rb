b6d69cea3b27ff8a589c3ff50ae7398699b98cce
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-14 14:41:57 -0700

    added params

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,10 @@ class CustomersController < ApplicationController
     render json: customers.as_json(only: [:name, :address, :registered_at, :city, :state, :postal_code, :phone]),
            status: :ok
   end
+
+  private
+
+  def customer_params
+    params.require(customer).permit(:name, :address, :registered_at, :city, :state, :postal_code, :phone)
+  end
 end
