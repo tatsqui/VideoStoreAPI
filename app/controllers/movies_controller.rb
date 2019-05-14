f2f45c67a6b4ebd48ff16d5e73183ce6700eb8d7
Tatiana Quintana <tatsqui@gmail.com>
2019-05-14 14:40:40 -0700

    made movie params private

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,8 @@ class MoviesController < ApplicationController
     render status: :ok, json: movies.as_json(only: [:title, :inventory, :overview, :release_date])
   end
 
+  private
+  
   def movie_params
     params.require(movie).permit(:title, :inventory, :overview, :release_date)
   end
