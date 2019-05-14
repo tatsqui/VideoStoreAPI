3ecdeedad3f93908eb3aae960117e496b6d300a5
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-14 15:04:37 -0700

    added movies checked out count column with a default of 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_185601) do
+ActiveRecord::Schema.define(version: 2019_05_14_220013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_185601) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movies_checked_out_count"
   end
 
   create_table "movies", force: :cascade do |t|
