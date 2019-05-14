9abc444e0d81fb7b026bcdd9b7cc24dfcc151ffe
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-14 14:28:07 -0700

    customer controller tests written and CRUD action wrritten, one test not passing

---
+++ test/controllers/customers_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+describe CustomersController do
+  describe "index" do
+    it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get customers_path
+      expect(response.header["Content-Type"]).must_include "json"
+    end
+
+    it "returns an Array" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the pets" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns pets with exactly the required fields" do
+      keys = %w(name address registered_at city state postal_code phone)
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
+  end
+end
