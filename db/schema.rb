266eb41eeb3633e4d05133f80fdc5cee246a2cac
Tatiana Quintana <tatsqui@gmail.com>
2019-05-14 16:42:06 -0700

    migrate customer checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_220005) do
-
+ActiveRecord::Schema.define(version: 2019_05_14_220013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
