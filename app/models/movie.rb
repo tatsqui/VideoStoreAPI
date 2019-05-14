742642bb32f9812c38da8f5edef8c3c609fa804e
Tatiana Quintana <tatsqui@gmail.com>
2019-05-14 12:26:21 -0700

    added model validation tests for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,3 @@
 class Movie < ApplicationRecord
-  
+  validates :title, :inventory, presence: true
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,7 +3,30 @@ require "test_helper"
 describe Movie do
   let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  describe "validations" do 
+    it "won't be valid without title and inventory" do
+      expect(movie).wont_be :valid?
+    end
+
+    it "will be valid with title and inventory" do 
+      movie.update_attributes({ title: "Test movie", inventory: 5 })
+
+      expect(movie).must_be :valid?
+    end
+
+    it "responds to its fields" do 
+      ["title", "overview", "release_date", "inventory"].each do |field|
+        expect(movie).must_respond_to field
+      end
+    end
+  end
+
+  describe "relationships" do 
+    it "" do 
+
+    end
+  end
+  
+  describe "custom methods" do 
   end
 end
