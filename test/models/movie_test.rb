60ca72fa38a1cef4dcb618d53b6ffcc65af883eb
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-15 15:54:46 -0700

    customer model method and test

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,39 +3,50 @@ require "test_helper"
 describe Movie do
   let(:movie) { Movie.new }
 
-  describe "validations and instantiation" do 
+  describe "validations and instantiation" do
     it "won't be valid without title and inventory" do
       expect(movie).wont_be :valid?
 
       # for title
       expect(movie.errors.messages).must_include :title
       expect(movie.errors.messages[:title]).must_equal ["can't be blank"]
-      
+
       # for inventory
       expect(movie.errors.messages).must_include :inventory
       expect(movie.errors.messages[:inventory]).must_equal ["can't be blank"]
     end
 
-    it "will be valid with title and inventory" do 
+    it "will be valid with title and inventory" do
       movie.update_attributes({ title: "Test movie", inventory: 5 })
 
       expect(movie).must_be :valid?
       expect(movie.errors.messages).must_be_empty
     end
 
-    it "responds to its fields" do 
+    it "responds to its fields" do
       ["title", "overview", "release_date", "inventory"].each do |field|
         expect(movie).must_respond_to field
       end
     end
   end
 
-  describe "relationships" do 
-    it "" do 
-
+  describe "relationships" do
+    it "" do
     end
   end
-  
-  describe "custom methods" do 
+
+  describe "custom methods" do
+    let(:movie) { movies(:movie_two) }
+
+    it "will increase the available inventory when a movie is checked in" do
+      amount = movie.available_inventory
+      movie.increase_available_inventory
+
+      movie.reload
+
+      new_amount = movie.available_inventory
+
+      expect(new_amount).must_equal amount + 1
+    end
   end
 end
