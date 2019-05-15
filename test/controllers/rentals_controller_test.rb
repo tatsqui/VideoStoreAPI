72e820950bc76273a3f41c1a369c8d4f5ffc450f
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-15 14:21:24 -0700

    contoller test shells

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "checkin" do
+    it "can checkin a movie if inventory is greater than available inventory" do
+    end
+
+    it "will increase the available inventory when a movie is checked in" do
+    end
+
+    it "can't checkin a movie if inventory equals available inventory" do
+    end
+
+    it "can't checkin a non existent movie" do
+    end
+
+    # it "can only checkin a movie that specific customer checked out" do
+    # end
+  end
 end
